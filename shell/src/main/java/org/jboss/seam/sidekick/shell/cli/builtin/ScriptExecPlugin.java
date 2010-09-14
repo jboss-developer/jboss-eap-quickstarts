9cd4cb49a02ee103b355c0a28e43c6c4129370a9
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2010-09-14 18:19:09 -0400

    minor update

--- shell/src/main/java/org/jboss/seam/sidekick/shell/cli/builtin/ScriptExecPlugin.java
+++ shell/src/main/java/org/jboss/seam/sidekick/shell/cli/builtin/ScriptExecPlugin.java
@@ -1,54 +1,53 @@
 package org.jboss.seam.sidekick.shell.cli.builtin;
 
+import static java.lang.String.valueOf;
+import static org.mvel2.MVEL.eval;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.jboss.seam.sidekick.shell.Shell;
-import org.jboss.seam.sidekick.shell.cli.PluginRegistry;
 import org.jboss.seam.sidekick.shell.plugins.DefaultCommand;
 import org.jboss.seam.sidekick.shell.plugins.Help;
 import org.jboss.seam.sidekick.shell.plugins.Option;
 import org.jboss.seam.sidekick.shell.plugins.Plugin;
 import org.mvel2.util.StringAppender;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import static java.lang.String.valueOf;
-import static org.mvel2.MVEL.eval;
-
 /**
- * User: christopherbrock
- * Date: 1-Sep-2010
- * Time: 5:04:36 PM
+ * User: christopherbrock Date: 1-Sep-2010 Time: 5:04:36 PM
  */
 @Named("exec")
 @Help("Executes an expression")
 public class ScriptExecPlugin implements Plugin
 {
    private final Shell shell;
-   private final PluginRegistry registry;
 
    @Inject
-   public ScriptExecPlugin(Shell shell, PluginRegistry registry)
+   public ScriptExecPlugin(final Shell shell)
    {
       this.shell = shell;
-      this.registry = registry;
    }
 
    @DefaultCommand
-   public void execScript(@Option(required = true, description = "expr") String... expr)
+   public void execScript(@Option(required = true, description = "expr") final String... expr)
    {
       StringAppender appender = new StringAppender();
       for (String s : expr)
+      {
          appender.append(s);
+      }
 
       Object retVal = eval(appender.toString(), new ScriptContext(), shell.getProperties());
 
       if (retVal != null)
+      {
          shell.println(valueOf(retVal));
+      }
    }
 
    public class ScriptContext
    {
-      public void cmd(String cmd)
+      public void cmd(final String cmd)
       {
          shell.execute(cmd);
       }
