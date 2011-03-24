e9478feb25bc21d528bdac5d6336e00489ab5128
Mike Brock <brockm@gmail.com>
2011-03-24 17:59:37 -0400

    fix to windows path parsing.

--- shell/src/test/java/org/jboss/seam/forge/shell/test/resources/ResourceAPITests.java
+++ shell/src/test/java/org/jboss/seam/forge/shell/test/resources/ResourceAPITests.java
@@ -1,22 +1,21 @@
 package org.jboss.seam.forge.shell.test.resources;
 
-import java.io.File;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import junit.framework.Assert;
-
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.seam.forge.project.services.ResourceFactory;
 import org.jboss.seam.forge.resources.DirectoryResource;
 import org.jboss.seam.forge.resources.Resource;
+import org.jboss.seam.forge.shell.util.OSUtils;
 import org.jboss.seam.forge.shell.util.PathspecParser;
 import org.jboss.seam.forge.shell.util.ResourceUtil;
 import org.jboss.seam.forge.test.AbstractShellTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.File;
+import java.util.List;
 
 @Singleton
 @RunWith(Arquillian.class)
@@ -29,7 +28,7 @@ public class ResourceAPITests extends AbstractShellTest
    public void testPathNavigation()
    {
       DirectoryResource expect = new DirectoryResource(factory, new File("").getAbsoluteFile().getParentFile()
-               .getParentFile());
+            .getParentFile());
       DirectoryResource r = new DirectoryResource(factory, new File("").getAbsoluteFile());
 
       Assert.assertEquals(expect, ResourceUtil.parsePathspec(factory, r, "../..").iterator().next());
@@ -54,6 +53,22 @@ public class ResourceAPITests extends AbstractShellTest
       Resource<?> actual = r.iterator().next();
 
       Assert.assertEquals(expect.getFullyQualifiedName(), actual.getFullyQualifiedName());
+   }
+
+   @Test
+   public void testWindowsPaths()
+   {
+      OSUtils.setPretendWindows(true);
+
+      DirectoryResource expect = new DirectoryResource(factory, new File("").getAbsoluteFile().getParentFile());
+
+      String name = expect.getName();
+
+      DirectoryResource r = new DirectoryResource(factory, new File("").getAbsoluteFile());
+
+      Assert.assertEquals(expect, ResourceUtil.parsePathspec(factory, r, "..\\..\\" + name).iterator().next());
+
+      OSUtils.setPretendWindows(false);
    }
 
    @Test
