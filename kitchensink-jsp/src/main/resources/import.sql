5fa8fbcf642f92869da33147524de29120bc611b
nelvadas <nelvadas@gmail.com>
2011-12-13 00:12:19 +0100

    update to kitchensink-jsp Table Member renamed  Table (name="MemberJSP", Loading query upadate Package upadate kitchensinkjsp

---
+++ kitchensink-jsp/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into MemberJSP (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.jsp.com', '2125551212') ;
+insert into MemberJSP (id, name, email, phone_number) values (1, 'Elvadas Nono', 'elvadas.nono@dgf.co', '1234567890'); 
