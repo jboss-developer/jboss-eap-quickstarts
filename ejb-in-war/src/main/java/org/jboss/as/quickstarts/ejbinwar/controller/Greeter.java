10d9d75663d65660d3a94438620ff37344688761
Paul Robinson <paul.robinson@redhat.com>
2011-12-23 18:40:51 +0000

    Changed comments to javadoc

--- ejb-in-war/src/main/java/org/jboss/as/quickstarts/ejbinwar/controller/Greeter.java
+++ ejb-in-war/src/main/java/org/jboss/as/quickstarts/ejbinwar/controller/Greeter.java
@@ -37,24 +37,31 @@ import java.io.Serializable;
 @SessionScoped
 public class Greeter implements Serializable {
 
-	/*
+	/**
 	 * Injected GreeterEJB client
 	 */
 	@EJB
 	private GreeterEJB greeterEJB;
 
-	/*
+	/**
 	 * Stores the response from the call to greeterEJB.sayHello(...)
 	 */
 	private String message;
 
-	/*
+	/**
 	 * Invoke greeterEJB.sayHello(...) and store the message
+     *
+     * @param name The name of the person to be greeted
 	 */
 	public void setName(String name) {
 		message = greeterEJB.sayHello(name);
 	}
 
+    /**
+     * Get the greeting message, customized with the name of the person to be greeted.
+     *
+     * @return message. The greeting message.
+     */
 	public String getMessage() {
 		return message;
 	}
