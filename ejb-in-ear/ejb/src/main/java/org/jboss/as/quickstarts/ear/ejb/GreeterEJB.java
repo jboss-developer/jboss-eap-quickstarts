fbd522eaaeb1a26000dc7239a4440887c7ca8c41
Paul Robinson <paul.robinson@redhat.com>
2011-12-23 19:10:38 +0000

    Changed comments to javadoc

--- ejb-in-ear/ejb/src/main/java/org/jboss/as/quickstarts/ear/ejb/GreeterEJB.java
+++ ejb-in-ear/ejb/src/main/java/org/jboss/as/quickstarts/ear/ejb/GreeterEJB.java
@@ -24,11 +24,19 @@ package org.jboss.as.quickstarts.ear.ejb;
 import javax.ejb.Stateful;
 
 /**
+ * A simple Hello World EJB. The EJB does not use an interface.
+ * 
  * @author paul.robinson@redhat.com, 2011-12-21
  */
 @Stateful
 public class GreeterEJB {
-
+	/**
+	 * This method takes a name and returns a personalised greeting.
+	 * 
+	 * @param name
+	 *            the name of the person to be greeted
+	 * @return the personalised greeting.
+	 */
 	public String sayHello(String name) {
 		return "Hello " + name;
 	}
--- ejb-in-ear/web/src/main/java/org/jboss/as/quickstarts/ear/controller/Greeter.java
+++ ejb-in-ear/web/src/main/java/org/jboss/as/quickstarts/ear/controller/Greeter.java
@@ -22,6 +22,7 @@
 package org.jboss.as.quickstarts.ear.controller;
 
 import org.jboss.as.quickstarts.ear.ejb.GreeterEJB;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -29,33 +30,42 @@ import javax.inject.Named;
 import java.io.Serializable;
 
 /**
- * A simple managed bean that is used to invoke the GreeterEJB and store the response. The response is obtained by
- * invoking getMessage().
- *
+ * A simple managed bean that is used to invoke the GreeterEJB and store the
+ * response. The response is obtained by invoking getMessage().
+ * 
  * @author paul.robinson@redhat.com, 2011-12-21
  */
 @Named("greeter")
 @SessionScoped
 public class Greeter implements Serializable {
 
-	/*
+	/**
 	 * Injected GreeterEJB client
 	 */
 	@EJB
 	private GreeterEJB greeterEJB;
 
-	/*
+	/**
 	 * Stores the response from the call to greeterEJB.sayHello(...)
 	 */
 	private String message;
 
-	/*
+	/**
 	 * Invoke greeterEJB.sayHello(...) and store the message
+	 * 
+	 * @param name
+	 *            The name of the person to be greeted
 	 */
 	public void setName(String name) {
 		message = greeterEJB.sayHello(name);
 	}
 
+	/**
+	 * Get the greeting message, customized with the name of the person to be
+	 * greeted.
+	 * 
+	 * @return message. The greeting message.
+	 */
 	public String getMessage() {
 		return message;
 	}
