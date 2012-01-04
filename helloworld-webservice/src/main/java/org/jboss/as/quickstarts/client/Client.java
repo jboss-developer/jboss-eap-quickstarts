cdad87f514cc093cb75ce921e169fcbe59c527e8
Serge Pagop <serge.pagop@googlemail.com>
2012-01-04 18:34:23 +0100

    2nd commit

--- helloworld-webservice/src/main/java/org/jboss/as/quickstarts/client/Client.java
+++ helloworld-webservice/src/main/java/org/jboss/as/quickstarts/client/Client.java
@@ -1,10 +1,7 @@
 package org.jboss.as.quickstarts.client;
 
 import java.net.URL;
-import java.util.Hashtable;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
@@ -18,18 +15,17 @@ import org.jboss.as.quickstarts.webservice.Calculator;
 public class Client {
 	public static void main(String[] args) throws Exception {
 
-		final Hashtable jndiProperties = new Hashtable();
-		jndiProperties.put(Context.URL_PKG_PREFIXES,
-				"org.jboss.ejb.client.naming");final Context context = new InitialContext(jndiProperties);
-
 		final QName serviceName = new QName("org.jboss.as.quickstarts.webservice", "CalculatorService");
 		final URL wsdlURL = new URL("http://localhost:8080/jboss-as-helloworld-webservice/CalculatorService/CalculatorBean?wsdl");
 
 		final Service service = Service.create(wsdlURL, serviceName);
 		final Calculator calculator = (Calculator) service
 				.getPort(Calculator.class);
-
-		System.out.println("1 + 1 = " + calculator.add(1, 1));
-		System.out.println("1 - 1 = " + calculator.subtract(1, 1));
+		System.out.println();
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+		System.out.println("ADD:  5 + 2 = " + calculator.add(5, 2));
+		System.out.println("DIFF: 1 - 1 = " + calculator.subtract(1, 1));
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+		System.out.println();
 	}
 }
