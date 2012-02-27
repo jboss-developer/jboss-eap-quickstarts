a8ac921455eb2c08894274e0ae8f69ac1046595f
Lukas Fryc <lfryc@redhat.com>
2012-02-27 16:55:58 +0100

    all view actions works as expected

--- tasks-jsf/src/main/java/org/jboss/as/quickstarts/tasksJsf/web/TaskController.java
+++ tasks-jsf/src/main/java/org/jboss/as/quickstarts/tasksJsf/web/TaskController.java
@@ -1,32 +1,27 @@
 package org.jboss.as.quickstarts.tasksJsf.web;
 
-import java.io.Serializable;
-
-import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.jboss.as.quickstarts.tasksJsf.beans.CurrentTaskStore;
 import org.jboss.as.quickstarts.tasksJsf.domain.Task;
 import org.jboss.as.quickstarts.tasksJsf.domain.TaskDao;
 import org.jboss.as.quickstarts.tasksJsf.domain.User;
-import org.jboss.as.quickstarts.tasksJsf.qualifiers.CurrentTask;
 import org.jboss.as.quickstarts.tasksJsf.qualifiers.CurrentUser;
 
 /**
  * <p>
- * Basic operations for tasks owned by current user - additions, deletions - and provides current selected task.
+ * Basic operations for tasks owned by current user - additions, deletions/
  * </p>
- *
+ * 
  * @author Lukas Fryc
- *
+ * 
  */
+@RequestScoped
 @Named("tasks")
-@ConversationScoped
-public class TaskController implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class TaskController {
 
     @Inject
     TaskDao taskDao;
@@ -39,65 +34,44 @@ public class TaskController implements Serializable {
      */
     @Inject
     @CurrentUser
-    User currentUser;
+    Instance<User> currentUser;
 
-    private Task currentTask;
-
-    /**
-     * <p>
-     * Provides current task to the context available for injection using:
-     * </p>
-     *
-     * <p>
-     * <code>@Inject @CurrentTask currentTask;</code>
-     * </p>
-     *
-     * <p>
-     * or from the Expression Language context using an expression <code>#{currentTask}</code>.
-     * </p>
-     *
-     * @return current authenticated user
-     */
-    @Produces
-    @CurrentTask
-    @Named
-    public Task getCurrentTask() {
-        return currentTask;
-    }
-
-    /**
-     * Setup current task
-     *
-     * @param currentTask task to setup as current
-     */
-    public void setCurrentTask(Task currentTask) {
-        this.currentTask = currentTask;
-    }
+    @Inject
+    CurrentTaskStore currentTaskStore;
 
     /**
      * Creates new task and, if no task is selected as current, selects it.
-     *
+     * 
      * @param taskTitle
      */
     public void createTask(String taskTitle) {
-        Task task = new Task(taskTitle);
         taskList.invalidate();
-        taskDao.createTask(currentUser, task);
-        if (currentTask == null) {
-            currentTask = task;
+        Task task = new Task(taskTitle);
+        taskDao.createTask(currentUser.get(), task);
+        if (currentTaskStore.get() == null) {
+            currentTaskStore.set(task);
         }
     }
 
     /**
      * Deletes given task
-     *
+     * 
      * @param task to delete
      */
     public void deleteTask(Task task) {
-        if (task.equals(currentTask)) {
-            currentTask = null;
-        }
         taskList.invalidate();
+        if (task.equals(currentTaskStore.get())) {
+            currentTaskStore.unset();
+        }
         taskDao.deleteTask(task);
+    }
+
+    /**
+     * Deletes given task
+     * 
+     * @param task to delete
+     */
+    public void deleteCurrentTask() {
+        deleteTask(currentTaskStore.get());
     }
 }
