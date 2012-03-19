b0f0a8f28cacb50deaa1c5465b6c44ae9baa8f40
Darrin Mison <dmison@me.com>
2012-03-19 12:00:47 +1000

    hopefully makes the reason for this a little clearer
    Signed-off-by: Darrin Mison <dmison@me.com>

--- logging-tools-qs/src/main/java/org/jboss/as/quickstarts/loggingToolsQS/exceptions/LocaleInvalidException.java
+++ logging-tools-qs/src/main/java/org/jboss/as/quickstarts/loggingToolsQS/exceptions/LocaleInvalidException.java
@@ -4,6 +4,18 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+/*
+ *  Wrapper class for WebApplicationException.  WebApplicationException doesn't 
+ *  use the `String message` parameter from Throwable which is required for the passing
+ *  of the translated message.
+ *  
+ *    This class simply provides a constructor with String message which is passed along
+ *    to the super class constructor as part of it's Response object.
+ *    
+ *    http://docs.oracle.com/javaee/6/api/javax/ws/rs/WebApplicationException.html
+ *    
+ */
+
 @SuppressWarnings("serial")
 public class LocaleInvalidException extends WebApplicationException 
 {
