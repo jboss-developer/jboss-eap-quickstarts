cda4fe8bf434b2e3b227dabd702a3ac6bc96a736
Fred Bricon <fbricon@gmail.com>
2012-10-17 17:09:18 +0200

    Add zip modules script and profile
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ zipmodules.bat
@@ -0,0 +1,2 @@
+ mvn groovy:execute -N -Pzip-modules,requires-postgres,complex-dependencies,requires-full,requires-xts 
+ 
---
+++ zipmodules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn groovy:execute -N -Pzip-modules,requires-postgres,complex-dependencies,requires-full,requires-xts 
+ 
