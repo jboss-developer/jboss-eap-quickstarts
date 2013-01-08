16d7bf92cf5aead63d995d83f00ba0691cab42ab
Lucas Holmquist <lholmqui@redhat.com>
2013-01-08 11:53:15 -0500

    Remove license year

--- kitchensink-html5-mobile/src/main/java/org/jboss/as/quickstarts/html5_mobile/data/MemberRepository.java
+++ kitchensink-html5-mobile/src/main/java/org/jboss/as/quickstarts/html5_mobile/data/MemberRepository.java
@@ -1,6 +1,6 @@
-/**
+/*
  * JBoss, Home of Professional Open Source
- * Copyright 2012, Red Hat, Inc., and individual contributors
+ * Copyright Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,  
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
