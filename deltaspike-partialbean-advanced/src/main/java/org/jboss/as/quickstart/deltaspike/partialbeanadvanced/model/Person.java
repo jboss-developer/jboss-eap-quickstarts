80da7f1de5e4870b86e5fb8d89550da0efb491da
Jesse Sightler <jsightle@redhat.com>
2013-02-11 18:35:23 -0500

    Added initial pass of InvocationHandler QuickStart.

---
+++ deltaspike-partialbean-advanced/src/main/java/org/jboss/as/quickstart/deltaspike/partialbeanadvanced/model/Person.java
@@ -0,0 +1,103 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.quickstart.deltaspike.partialbeanadvanced.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * This is a typical {@link Entity} with the field <i>nick</i> as its {@link Id}
+ * 
+ * @author <a href="mailto:benevides@redhat.com">Rafael Benevides</a>
+ * 
+ */
+@Entity
+@Table(name = "person")
+public class Person {
+	@Id
+    private String nick;
+
+    private String name;
+
+	public Person() {
+	}
+	
+	public Person(String nick, String name) {
+		super();
+		this.nick = nick;
+		this.name = name;
+	}
+    
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	@Override
+	public String toString() {
+		return "Person [nick=" + nick + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((nick == null) ? 0 : nick.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (nick == null) {
+			if (other.nick != null)
+				return false;
+		} else if (!nick.equals(other.nick))
+			return false;
+		return true;
+	}
+}
---
+++ deltaspike-partialbean-advanced/src/main/java/org/jboss/as/quickstart/deltaspike/partialbeanadvanced/queryservice/PersonQueryService.java
@@ -0,0 +1,60 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstart.deltaspike.partialbeanadvanced.queryservice;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+
+import org.jboss.as.quickstart.deltaspike.partialbeanadvanced.model.Person;
+
+/**
+ * Sample query service for the Person Entity. 
+ *
+ * This provides a number of query methods, that are implemented by the 
+ * QueryServicePartialBean. The entire query is configured
+ * via the @QueryMethod annotation.
+ */
+@RequestScoped
+@QueryServiceBinding
+public interface PersonQueryService {
+	/**
+	 * Returns all person entries in the Person table
+	 */
+	@QueryMethod("select p from Person p")
+	public List<Person> findAll();
+	
+	/**
+	 * Returns all person entries in the Person table, sorted
+	 * by person.name
+	 */
+	@QueryMethod("select p from Person p order by p.name")
+	public List<Person> findAllSortedByName();
+	
+	/**
+	 * Returns the person entry with the given nick
+	 */
+	@QueryMethod(value = "select p from Person p where p.nick = ?", singleResult=true)
+	public Person findPersonByNick(String nick);
+	
+	/**
+	 * Returns all person entries in the Person table, filtered
+	 * by person.name
+	 */
+	@QueryMethod("select p from Person p where p.name = ?")
+	public List<Person> findByName(String name);
+}
