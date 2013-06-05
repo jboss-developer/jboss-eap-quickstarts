fc3a57da3a53b46f1751b8d305e4a44dd158799b
pedroigor <pigor.craveiro@gmail.com>
2013-06-05 19:11:23 -0300

    [JDF-295] - Refactoring packages.

--- picketlink-authentication-http-basic/src/main/java/org/jboss/as/quickstarts/picketlink/authentication/basic/IDMInitializer.java
+++ picketlink-authentication-http-basic/src/main/java/org/jboss/as/quickstarts/picketlink/authentication/basic/IDMInitializer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.as.quickstarts.picketlink.authentication.idm.jsf;
+package org.jboss.as.quickstarts.picketlink.authentication.basic;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
