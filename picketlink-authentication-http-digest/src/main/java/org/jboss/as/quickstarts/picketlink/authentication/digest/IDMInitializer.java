78ccefdb4427a3bab12f629499742d7baacbcc10
pedroigor <pigor.craveiro@gmail.com>
2013-06-05 19:12:13 -0300

    [JDF-296] - Refactoring packages.

--- picketlink-authentication-http-digest/src/main/java/org/jboss/as/quickstarts/picketlink/authentication/digest/IDMInitializer.java
+++ picketlink-authentication-http-digest/src/main/java/org/jboss/as/quickstarts/picketlink/authentication/digest/IDMInitializer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.as.quickstarts.picketlink.authentication.idm.jsf;
+package org.jboss.as.quickstarts.picketlink.authentication.digest;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
