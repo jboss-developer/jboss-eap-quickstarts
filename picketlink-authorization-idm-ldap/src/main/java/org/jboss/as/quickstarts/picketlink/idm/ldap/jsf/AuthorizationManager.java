157921a6b86def4e31b22e9d695f01cfd85a025a
pedroigor <pigor.craveiro@gmail.com>
2013-06-28 13:12:16 -0300

    [JDF-288] - Add a quickstart that shows authentication and authorization against LDAP.

---
+++ picketlink-authorization-idm-ldap/src/main/java/org/jboss/as/quickstarts/picketlink/idm/ldap/jsf/AuthorizationManager.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.picketlink.idm.ldap.jsf;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.picketlink.Identity;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.model.Agent;
+import org.picketlink.idm.model.Role;
+
+/**
+ * <p>This class centralizes all authorization services for this application.</p>
+ * 
+ * @author pedroigor
+ * 
+ */
+@ApplicationScoped
+@Named
+public class AuthorizationManager {
+
+    @Inject
+    private Instance<Identity> identityInstance;
+
+    @Inject
+    private IdentityManager identityManager;
+
+    private boolean hasRole(ApplicationRole applicationRole) {
+        Agent agent = getIdentity().getAgent();
+        Role role = this.identityManager.getRole(applicationRole.name());
+
+        return this.identityManager.hasRole(agent, role);
+    }
+
+    public boolean isRisksManagementAllowed() {
+        return isAdministrator() || (isProjectManager());
+    }
+
+    public boolean isTimesheetAllowed() {
+        return isAdministrator() || (isProjectManager() || isDeveloper());
+    }
+
+    public boolean isSystemAdministrationAllowed() {
+        return isAdministrator();
+    }
+
+    public boolean isProjectManager() {
+        return hasRole(ApplicationRole.PROJECT_MANAGER);
+    }
+
+    public boolean isAdministrator() {
+        return hasRole(ApplicationRole.ADMINISTRATOR);
+    }
+
+    public boolean isDeveloper() {
+        return hasRole(ApplicationRole.DEVELOPER);
+    }
+
+    private Identity getIdentity() {
+        return this.identityInstance.get();
+    }
+}
---
+++ picketlink-authorization-idm-ldap/src/main/java/org/jboss/as/quickstarts/picketlink/idm/ldap/jsf/IDMInitializer.java
@@ -0,0 +1,79 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.quickstarts.picketlink.idm.ldap.jsf;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.credential.Password;
+import org.picketlink.idm.model.Role;
+import org.picketlink.idm.model.SimpleRole;
+import org.picketlink.idm.model.SimpleUser;
+import org.picketlink.idm.model.User;
+import static org.jboss.as.quickstarts.picketlink.idm.ldap.jsf.ApplicationRole.ADMINISTRATOR;
+import static org.jboss.as.quickstarts.picketlink.idm.ldap.jsf.ApplicationRole.DEVELOPER;
+import static org.jboss.as.quickstarts.picketlink.idm.ldap.jsf.ApplicationRole.PROJECT_MANAGER;
+
+@Startup
+@Singleton
+public class IDMInitializer {
+
+    @Inject
+    private IdentityManager identityManager;
+
+    @PostConstruct
+    public void createDefaultUsers() {
+        createUser("admin", ADMINISTRATOR);
+        createUser("john", PROJECT_MANAGER);
+        createUser("kate", DEVELOPER);
+    }
+
+    private void createUser(String loginName, ApplicationRole roleName) {
+        User user = this.identityManager.getUser(loginName);
+
+        if (user == null) {
+            user = new SimpleUser(loginName);
+
+            this.identityManager.add(user);
+
+            Password password = new Password(loginName + "123");
+
+            this.identityManager.updateCredential(user, password);
+        }
+
+        Role role = this.identityManager.getRole(roleName.name());
+
+        if (role == null) {
+            role = new SimpleRole(roleName.name());
+
+            this.identityManager.add(role);
+        }
+
+        if (!this.identityManager.hasRole(user, role)) {
+            this.identityManager.grantRole(user, role);
+        }
+    }
+
+}
