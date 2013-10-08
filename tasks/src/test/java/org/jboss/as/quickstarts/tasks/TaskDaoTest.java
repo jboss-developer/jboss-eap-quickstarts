d83b0e2c80d63ee442c0beb0d62ef74a12697f49
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-10-08 20:27:37 +0530

    BZ#1015948 Fixed test failures.
    Adopted use of @InSequence annotation to order tests for TaskDao.
    
    Converted the Resources bean in tasks-jsf to a @RequestScoped one
    to ensure that Arquillian injects a valid EntityManager instance
    in the test instances.

--- tasks/src/test/java/org/jboss/as/quickstarts/tasks/TaskDaoTest.java
+++ tasks/src/test/java/org/jboss/as/quickstarts/tasks/TaskDaoTest.java
@@ -27,6 +27,7 @@ import javax.persistence.EntityManager;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.Test;
@@ -59,6 +60,7 @@ public class TaskDaoTest {
     }
 
     @Test
+    @InSequence(1)
     public void user_should_be_created_with_one_task_attached() throws Exception {
         // given
         User user = new User("New user");
@@ -76,6 +78,7 @@ public class TaskDaoTest {
     }
 
     @Test
+    @InSequence(2)
     public void all_tasks_should_be_obtained_from_detachedUser() {
         // when
         List<Task> userTasks = taskDao.getAll(detachedUser);
@@ -85,6 +88,7 @@ public class TaskDaoTest {
     }
 
     @Test
+    @InSequence(3)
     public void range_of_tasks_should_be_provided_by_taskDao() {
         // when
         List<Task> headOfTasks = taskDao.getRange(detachedUser, 0, 1);
@@ -98,6 +102,7 @@ public class TaskDaoTest {
     }
 
     @Test
+    @InSequence(4)
     public void taskDao_should_provide_basic_case_insensitive_full_text_search() {
         // given
         String taskTitlePart = "FIRST";
@@ -111,6 +116,7 @@ public class TaskDaoTest {
     }
 
     @Test
+    @InSequence(5)
     public void taskDao_should_remove_task_from_detachedUser() {
         // given
         Task task = new Task();
