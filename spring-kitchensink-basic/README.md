0a6faa866166dc8a2941d1062071147bf6fcd9e2
Joshua Wilson <javajoshw@gmail.com>
2014-01-29 23:20:36 -0500

    WFK2-392 Rename all spring quickstarts to start with spring.

---
+++ spring-kitchensink-basic/README.md
@@ -0,0 +1,168 @@
+spring-kitchensink-basic: Kitchensink Example using Spring 3.2
+==============================================================
+Author: Marius Bogoevici, Tejas Mehta, Joshua Wilson  
+Level: Intermediate  
+Technologies: JSP, JPA, JSON, Spring, JUnit  
+Summary: An example that incorporates multiple technologies  
+Target Product: WFK  
+Product Versions: EAP 6.1, EAP 6.2, WFK 2.4  
+Source: <https://github.com/jboss-developer/jboss-wfk-quickstarts/>  
+
+What is it?
+-----------
+
+
+This is your project! It is a sample, deployable Maven 3 project to help you get your foot in the door developing with 
+Java EE 6 and Spring on Red Hat JBoss Enterprise Application Platform 6.1 or later.
+
+This project is setup to allow you to create a compliant Java EE 6 application using JSP, JPA 2.0 and Spring 3.2. It 
+includes a persistence unit and some sample persistence and transaction code to introduce you to database access in enterprise Java:
+
+* In `jboss-as-spring-mvc-context.xml` `<context:component-scan base-package="org.jboss.as.quickstarts.kitchensink.spring.basic.controller"/>` and `<mvc:annotation-driven/>` are used to register both the non-rest and rest controllers.
+
+* The controllers map the respective urls to methods using `@RequestMapping(url)`.
+
+* To return JSON, the rest controller uses `@ResponseBody`.
+
+* The datasource and entitymanager are retrieved via JNDI.
+
+System Requirements
+-------------------
+
+The application this project produces is designed to be run on Red Hat JBoss Enterprise Application Platform (EAP) 6.1 or 
+later with the Red Hat JBoss Web Framework Kit (WFK) 2.4.
+
+To run the quickstart with the provided build script, you need the following:
+
+1. Java 1.6, to run JBoss and Maven. You can choose from the following:
+    * OpenJDK
+    * Oracle Java SE
+    * Oracle JRockit
+
+2. Maven 3.0.0 or later, to build and deploy the examples
+    * If you have not yet installed Maven, see the [Maven Getting Started Guide](http://maven.apache.org/guides/getting-started/index.html) for details.
+    * If you have installed Maven, you can check the version by typing the following in a command line:
+
+            mvn --version 
+
+3. The JBoss EAP distribution ZIP.
+    * For information on how to install and run JBoss, refer to the product documentation.
+
+
+Configure Maven
+---------------
+
+If you have not yet done so, you must [Configure Maven](../README.md#configure-maven) before testing the quickstarts.
+
+
+Start the JBoss Server
+-------------------------
+
+1. Open a command line and navigate to the root of the JBoss server directory.
+2. The following shows the command line to start the server:
+
+        For Linux:   JBOSS_HOME/bin/standalone.sh
+        For Windows: JBOSS_HOME\bin\standalone.bat
+
+ 
+Build and Deploy the Quickstart
+-------------------------
+
+_NOTE: The following build command assumes you have configured your Maven user settings. If you have not, you must include Maven setting arguments on the command line. See [Build and Deploy the Quickstarts](../README.md#build-and-deploy-the-quickstarts) for complete instructions and additional options._
+
+1. Make sure you have started the JBoss Server as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. Type this command to build and deploy the archive:
+
+        mvn clean install jboss-as:deploy
+
+4. This will deploy `target/jboss-spring-kitchensink-basic.war` to the running instance of the server.
+
+
+Access the application
+----------------------
+
+The application will be running at the following URL: <http://localhost:8080/jboss-spring-kitchensink-basic/>.
+
+
+Undeploy the Archive
+--------------------
+
+1. Make sure you have started the JBoss Server as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. When you are finished testing, type this command to undeploy the archive:
+
+        mvn jboss-as:undeploy
+
+Run the Arquillian Functional Tests
+-----------------------------------
+
+This quickstart provides Arquillian functional tests as well. They are located in the functional-tests/ subdirectory under the root directory of this quickstart.
+Functional tests verify that your application behaves correctly from the user's point of view. The tests open a browser instance, simulate clicking around the page as a normal user would do, and then close the browser instance.
+
+To run these tests, you must build the main project as described above.
+
+1. Open a command line and navigate to the root directory of this quickstart.
+2. Build the quickstart WAR using the following command:
+
+        mvn clean package
+
+3. Navigate to the functional-tests/ directory in this quickstart.
+4. If you have a running instance of the JBoss Server, as described above, run the remote tests by typing the following command:
+
+        mvn clean verify -Parq-jbossas-remote
+
+5. If you prefer to run the functional tests using managed instance of the JBoss server, meaning the tests will start the server for you, type fhe following command:
+
+        mvn clean verify -Parq-jbossas-managed
+
+Run the Arquillian Tests 
+-------------------------
+
+This quickstart provides Arquillian tests. By default, these tests are configured to be skipped as Arquillian tests require the use of a container. 
+
+_NOTE: The following commands assume you have configured your Maven user settings. If you have not, you must include Maven setting arguments on the command line. See [Run the Arquillian Tests](../README.md#run-the-arquillian-tests) for complete instructions and additional options._
+
+1. Make sure you have started the JBoss Server as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. Type the following command to run the test goal with the following profile activated:
+
+        mvn clean test -Parq-jbossas-remote 
+
+
+
+Run the Quickstart in JBoss Developer Studio or Eclipse
+-------------------------------------
+
+You can also start the server and deploy the quickstart from Eclipse using JBoss tools. For more information, see 
+[Use JBoss Developer Studio or Eclipse to Run the Quickstart](../README.md#use-jboss-developer-studio-or-eclipse-to-run-the-quickstarts)
+
+
+Resolve ERRORs in JBDS/Eclipse
+--------------------------------
+If you only build with Maven command line, you should not see any errors. However, if you `Build` or `Build Automatically` 
+with Eclipse, you will see some errors. The following lists the errors and how to fix them.
+ 
+1.  `cvc-complex-type.2.4.a: Invalid content was found starting with element 'deployment'. One of '{ear-subdeployments-isolated, deployment, sub-deployment, module}' is expected.`
+    *  Please ignore this error as it is due to a problem in the schema definition.  
+2.  `Console configuration "" does not exist.`
+    1.  Select the project 
+    2.  Select the `Project` menu, then `Properties` -> `Hibernate Settings`
+        1.  Look in the combo-box below the "Default Hibernate Console configuration:" label and select a console configuration. 
+        2.  If no console configuration exists then Cancel and do the following.
+            1.  Select `Run` -> `Run Configurations` 
+            2.  Under `Hibernate Console Configuration` create a new Configuration
+            3.  Enter the project name as the Name
+            4.  In the "Database connection:" combo-box select "[JPA Project Configured Connection]"
+            5.  Select `Apply`
+            6.  Return to step 1 to select the configuration you just created.
+
+
+Debug the Application
+---------------------
+
+If you want to debug the source code or look at the Javadocs of any library in the project, run either of the following 
+commands to pull them into your local repository. The IDE should then detect them.
+
+        mvn dependency:sources
+        mvn dependency:resolve -Dclassifier=javadoc
---
+++ spring-kitchensink-springmvctest/functional-tests/src/test/java/org/jboss/as/quickstarts/kitchensink/test/KitchensinkSpringMVCTest.java
@@ -0,0 +1,273 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.kitchensink.test;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.findby.FindByJQuery;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.net.URL;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.jboss.arquillian.graphene.Graphene.guardHttp;
+
+/**
+ * Kitchensink Spring MVC quickstart functional test
+ *
+ * @author Oliver Kiss
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class KitchensinkSpringMVCTest {
+
+    /**
+     * Injects browser to our test.
+     */
+    @Drone
+    WebDriver browser;
+
+    /**
+     * Injects URL on which application is running.
+     */
+    @ArquillianResource
+    URL contextPath;
+
+    /**
+     * Creates deployment which is sent to the container upon test's start.
+     *
+     * @return war file which is deployed while testing, the whole application in our case
+     */
+    @Deployment(testable = false)
+    public static WebArchive deployment() {
+        return Deployments.kitchensink();
+    }
+
+    /**
+     * Locator for name field
+     */
+    @FindBy(id = "name")
+    WebElement nameField;
+
+    /**
+     * Locator for email field
+     */
+    @FindBy(id = "email")
+    WebElement emailField;
+
+    /**
+     * Locator for phone number field
+     */
+    @FindBy(id = "phoneNumber")
+    WebElement phoneField;
+
+    /**
+     * Locator for registration button
+     */
+    @FindByJQuery("input.register")
+    WebElement registerButton;
+
+    /**
+     * Locator for rows of the members table
+     */
+    @FindByJQuery("table.simpletablestyle:first tbody tr")
+    List<WebElement> tableMembersRows;
+
+    /**
+     * Locator for columns of the first row of the members table
+     */
+    @FindByJQuery("table.simpletablestyle:first tbody tr:first td")
+    List<WebElement> tableMembersRowColumns;
+
+    /**
+     * Locator for name field validation message
+     */
+    @FindBy(id = "name.errors")
+    WebElement nameErrorMessage;
+
+    /**
+     * Locator for email field validation message
+     */
+    @FindBy(id = "email.errors")
+    WebElement emailErrorMessage;
+
+    /**
+     * Locator for phone number field validation message
+     */
+    @FindBy(id = "phoneNumber.errors")
+    WebElement phoneErrorMessage;
+
+    /**
+     * Name of the member to register in the right format.
+     */
+    private static final String NAME_FORMAT_OK = "John Doe";
+
+    /**
+     * Name of the member to register in the bad format.
+     */
+    private static final String NAME_FORMAT_BAD = "John1";
+
+    /**
+     * Name of the member to register which is too long (1-25)
+     */
+    private static final String NAME_FORMAT_TOO_LONG = "John Doe John Doe John Doe";
+
+    /**
+     * E-mail of the member to register in the right format.
+     */
+    private static final String EMAIL_FORMAT_OK = "john@doe.com";
+
+    /**
+     * E-mail of the member to register in the bad format - #1.
+     */
+    private static final String EMAIL_FORMAT_BAD_1 = "joe";
+
+    /**
+     * E-mail of the member to register in the bad format - #2.
+     */
+    private static final String EMAIL_FORMAT_BAD_2 = "john@doe.com ";
+
+    /**
+     * Phone number of the member to register in the right format.
+     */
+    private static final String PHONE_FORMAT_OK = "0123456789";
+
+    /**
+     * Phone number of the member to register in the bad format - illegal
+     * characters.
+     */
+    private static final String PHONE_FORMAT_BAD_ILLEGAL_CHARS = "as/df.123@";
+
+    /**
+     * Phone number of the member to register in the bad format - too long.
+     */
+    private static final String PHONE_FORMAT_BAD_TOO_LONG = "12345678901234567890";
+
+    /**
+     * Phone nuber of the member to register in the bad format - too short
+     */
+    private static final String PHONE_FORMAT_BAD_TOO_SHORT = "123456789";
+
+
+    @Test
+    @InSequence(1)
+    public void testEmptyRegistration() {
+        browser.get(contextPath.toString());
+        guardHttp(registerButton).click();
+        assertTrue("Name validation message should be present", nameErrorMessage.isDisplayed());
+        assertTrue("Email validation message should be present", emailErrorMessage.isDisplayed());
+        assertTrue("PhoneNumber validation message should be present", phoneErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+    }
+
+    @Test
+    @InSequence(2)
+    public void testRegistrationWithBadNameFormat() {
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_BAD, EMAIL_FORMAT_OK, PHONE_FORMAT_OK);
+        guardHttp(registerButton).click();
+        assertTrue("Name validation message should be present", nameErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_TOO_LONG, EMAIL_FORMAT_OK, PHONE_FORMAT_OK);
+        guardHttp(registerButton).click();
+        assertTrue("Name validation message should be present", nameErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+    }
+
+    @Test
+    @InSequence(3)
+    public void testRegistrationWithBadEmailFormat() {
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_BAD_1, PHONE_FORMAT_OK);
+        guardHttp(registerButton).click();
+        assertTrue(emailErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_BAD_2, PHONE_FORMAT_OK);
+        guardHttp(registerButton).click();
+        assertTrue(emailErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+    }
+
+    @Test
+    @InSequence(4)
+    public void testRegistrationWithBadPhoneFormat() {
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_OK, PHONE_FORMAT_BAD_ILLEGAL_CHARS);
+        guardHttp(registerButton).click();
+        assertTrue(phoneErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_OK, PHONE_FORMAT_BAD_TOO_SHORT);
+        guardHttp(registerButton).click();
+        assertTrue(phoneErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_OK, PHONE_FORMAT_BAD_TOO_LONG);
+        guardHttp(registerButton).click();
+        assertTrue(phoneErrorMessage.isDisplayed());
+        assertEquals("Member should not be registered", 1, tableMembersRows.size());
+    }
+
+    @Test
+    @InSequence(5)
+    public void testRegularRegistration() {
+        browser.get(contextPath.toString());
+        setInputFields(NAME_FORMAT_OK, EMAIL_FORMAT_OK, PHONE_FORMAT_OK);
+        guardHttp(registerButton).click();
+
+        assertEquals(2, tableMembersRows.size());
+        assertEquals(5, tableMembersRowColumns.size());
+
+        assertTrue((tableMembersRowColumns.get(1)).getText().equals(NAME_FORMAT_OK));
+        assertTrue((tableMembersRowColumns.get(2)).getText().equals(EMAIL_FORMAT_OK));
+        assertTrue((tableMembersRowColumns.get(3)).getText().equals(PHONE_FORMAT_OK));
+    }
+
+    /**
+     * This helper method sets values into the according input fields.
+     *
+     * @param name  name to set into the name input field
+     * @param email email to set into the email input field
+     * @param phone phone to set into the phone input field
+     */
+    private void setInputFields(String name, String email, String phone) {
+        nameField.clear();
+        nameField.sendKeys(name);
+        emailField.clear();
+        emailField.sendKeys(email);
+        phoneField.clear();
+        phoneField.sendKeys(phone);
+    }
+
+}
