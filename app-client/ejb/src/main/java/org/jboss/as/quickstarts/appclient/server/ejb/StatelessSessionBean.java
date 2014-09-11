6669558177786ed4df15b5a41e3ed1fc0ac974dd
Sande Gilda <sgilda@redhat.com>
2014-09-11 15:53:02 -0400

    Fix QS tools issues in app-client quickstart

--- app-client/ejb/src/main/java/org/jboss/as/quickstarts/appclient/server/ejb/StatelessSessionBean.java
+++ app-client/ejb/src/main/java/org/jboss/as/quickstarts/appclient/server/ejb/StatelessSessionBean.java
@@ -30,17 +30,17 @@ import org.jboss.logging.Logger;
  */
 @Stateless
 public class StatelessSessionBean implements StatelessSession {
-  private static final Logger LOGGER = Logger.getLogger(StatelessSessionBean.class);
-  @Resource
-  SessionContext context;
-  
-  @Override
-  public void invokeWithClientContext() {
-    LOGGER.info("ClientContext is here = "+context.getContextData());
-  }
-  
-  @Override
-  public String getGreeting() {
-      return "Hello from StatelessSessionBean@" + System.getProperty("jboss.node.name");
-  }
+    private static final Logger LOGGER = Logger.getLogger(StatelessSessionBean.class);
+    @Resource
+    SessionContext context;
+    
+    @Override
+    public void invokeWithClientContext() {
+        LOGGER.info("ClientContext is here = "+context.getContextData());
+    }
+    
+    @Override
+    public String getGreeting() {
+        return "Hello from StatelessSessionBean@" + System.getProperty("jboss.node.name");
+    }
 }
