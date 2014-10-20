e1b560b2a3cdeb30b3bbb54c50ee67ed09555c33
Sande Gilda <sgilda@redhat.com>
2014-10-20 13:01:11 -0400

    Bz1153061: Modify stylesheet URL in ruby script that generates HTML from Markdown

--- dist/github-flavored-markdown.rb
+++ dist/github-flavored-markdown.rb
@@ -142,7 +142,7 @@ def markdown(source_path)
   rendered = markdown.render(text)
   metadata(source_path.path, rendered)
   rendered = rendered.gsub(/README.md/, "README.html").gsub(/CONTRIBUTING.md/, "CONTRIBUTING.html")
-  '<!DOCTYPE html><html><head><title>README</title><link href="https://github.com/jboss-developer/jboss-developer-shared-resources/blob/master/dist/css/documentation.css" rel="stylesheet"></link><link href="https://github.com/jboss-developer/jboss-developer-shared-resources/blob/master/dist/css/pygments.css" rel="stylesheet"></link></head><body>' + rendered + '</body></html>'
+  '<!DOCTYPE html><html><head><title>README</title><link href="http://static.jboss.org/ffe/0/www/vendor/redhat/documentation.css" rel="stylesheet"></link><link href="http://static.jboss.org/ffe/0/www/vendor/redhat/pygments.css" rel="stylesheet"></link></head><body>' + rendered + '</body></html>'
   end
 
 def optionize(options)
