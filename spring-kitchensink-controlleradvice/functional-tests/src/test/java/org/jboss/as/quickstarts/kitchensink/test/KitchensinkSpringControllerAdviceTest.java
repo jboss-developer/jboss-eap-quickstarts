77542d4c47a9ff10ca4749d2da81e662f764b397
Matous Jobanek <mjobanek@redhat.com>
2014-11-06 16:19:26 +0100

    changes in order to remove warnings in JBDS

--- spring-kitchensink-controlleradvice/functional-tests/src/test/java/org/jboss/as/quickstarts/kitchensink/test/KitchensinkSpringControllerAdviceTest.java
+++ spring-kitchensink-controlleradvice/functional-tests/src/test/java/org/jboss/as/quickstarts/kitchensink/test/KitchensinkSpringControllerAdviceTest.java
@@ -19,7 +19,6 @@ package org.jboss.as.quickstarts.kitchensink.test;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
-import org.jboss.arquillian.graphene.findby.ByJQuery;
 import org.jboss.arquillian.graphene.findby.FindByJQuery;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
@@ -30,11 +29,9 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
-import javax.swing.*;
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
