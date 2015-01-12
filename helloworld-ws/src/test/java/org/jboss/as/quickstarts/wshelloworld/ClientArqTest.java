f4b60e1676952552af645cd8947a735334622e5d
Sande Gilda <sgilda@redhat.com>
2015-01-12 09:01:44 -0500

    Bz1181116: helloworld-ws - Remove reference to obsolete web.xml in Arquillian tests

--- helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/ClientArqTest.java
+++ helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/ClientArqTest.java
@@ -18,7 +18,6 @@ package org.jboss.as.quickstarts.wshelloworld;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -42,10 +41,6 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class ClientArqTest {
     /**
-     * The location of the WebApp source folder so we know where to find the web.xml when deploying using Arquillian.
-     */
-    private static final String WEBAPP_SRC = "src/main/webapp";
-    /**
      * The name of the WAR Archive that will be used by Arquillian to deploy the application.
      */
     private static final String APP_NAME = "jboss-helloworld-ws";
@@ -61,8 +56,7 @@ public class ClientArqTest {
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, APP_NAME + ".war").addPackage(HelloWorldService.class.getPackage())
-                .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
+        return ShrinkWrap.create(WebArchive.class, APP_NAME + ".war").addPackage(HelloWorldService.class.getPackage());
     }
 
     @Before
