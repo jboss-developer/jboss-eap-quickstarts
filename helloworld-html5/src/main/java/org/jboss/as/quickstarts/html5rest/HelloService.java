1f084149e2e609358ac226e74c743fe2fe2953b8
Vinay2016 <Vinay2016@DESKTOP-H68KF86>
2016-05-26 17:07:10 -0700

    testasdfas

--- helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloService.java
+++ helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.html5rest;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello 428 : " + name + "!";
+        return "Hello 4289: " + name + "!";
     }
 
 }
