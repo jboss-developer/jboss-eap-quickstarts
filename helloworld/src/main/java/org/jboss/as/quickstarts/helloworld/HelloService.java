3866664986fb2d98701a2b53681d333220c3f3ba
ulttrix69 <59686513+ulttrix69@users.noreply.github.com>
2020-01-09 11:11:40 +0100

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello " + name + "!";
+        return "Hello " + name + " v.0.0.1 !";
     }
 
 }
