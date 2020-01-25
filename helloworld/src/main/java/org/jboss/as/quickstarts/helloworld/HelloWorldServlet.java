3c4cd45f69faef10e2a7e1717404f5f42ef3e510
Yalaoui <Yazid.Yalaoui-ext@sanofi.com>
2020-01-25 10:28:19 +0100

    change header 0.O.8

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -43,7 +43,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorldServlet extends HttpServlet {
 
-    static String PAGE_HEADER = "<html><head><title>helloworld  v.0.0.6 </title></head><body>";
+    static String PAGE_HEADER = "<html><head><title>helloworld  v.0.0.8 </title></head><body>";
 
     static String PAGE_FOOTER = "</body></html>";
 
