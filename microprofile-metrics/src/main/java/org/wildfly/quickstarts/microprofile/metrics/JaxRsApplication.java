52d4e4a1f7f6eb4525a323a22918be2332c2ff6d
Eduardo Martins <emartins@redhat.com>
2020-12-03 15:45:02 +0000

    EAP 7.4: remove XP quickstarts and dependencies

--- microprofile-metrics/src/main/java/org/wildfly/quickstarts/microprofile/metrics/JaxRsApplication.java
+++
@@ -1,8 +0,0 @@
-package org.wildfly.quickstarts.microprofile.metrics;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class JaxRsApplication extends Application {
-}
--- microprofile-rest-client/country-server/src/main/java/org/wildfly/quickstarts/microprofile/JaxRsApplication.java
+++
@@ -1,8 +0,0 @@
-package org.wildfly.quickstarts.microprofile;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class JaxRsApplication extends Application {
-}
